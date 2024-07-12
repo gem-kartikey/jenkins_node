f31cd9dbab29bc7e1b58c11f85276ab57474f78e
Kartikey Varshney <Kartikey.Varshney@Geminisolutions.com>
2024-07-12 11:45:44 +0530

    update 4

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/',(req,res)=>{
     res.send("this is response..")
 })
 
-app.listen(5000,(err)=>{
+app.listen(5050,(err)=>{
     (err)?console.log(err.message):console.log(`server is online at http://localhost:5000`)
 })
